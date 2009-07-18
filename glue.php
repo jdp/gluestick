0d22f405bd4ae34e0fff65f8016098baf44e5a20
jdp <jdp34@njit.edu>
2009-07-17 20:08:00 -0400

    Added GlueAsync for performing asynchronous calls

--- glue.php
+++ glue.php
@@ -1,48 +1,94 @@
 <?php
-class Glue {
-  private $__username;
-  private $__password;
-  private $__method_family;
-  private $credentials;
-  private $http_status;
-  private $last_api_call;
+class GlueException extends Exception {
+	var $code;
+	var $name;
+}
 
-  function __construct($username, $password, $method_family = null) {
-    $this->__username = $username;
-    $this->__password = $password;
-    if (isset($method_family)) {
-      $this->__method_family = $method_family;
-    }
-    else {
-      unset($this->__method_family);
-    }
-    $this->credentials = sprintf('%s:%s', $username, $password);
-  }
+abstract class Gluestick {
 
-  function __get($var) {
-    $class_name = get_class($this);
-    $ref = new $class_name($this->__username, $this->__password, $var);
-    return $ref;
-  }
+	protected $__username;
+	protected $__password;
+	protected $__method_family;
+	protected $credentials;
+	protected $http_status;
+	protected $last_api_call;
 
-  function __call($name, $args) {
-    $fields = http_build_query((count($args) > 0) ? $args[0] : array());
-    $method = (isset($this->__method_family)) ? sprintf('%s/%s', $this->__method_family, $name) : $name;
-    $api_call = sprintf('http://api.getglue.com/v1/%s?%s', $method, $fields);
-    return $this->request($api_call);
-  }
+	function __construct($username, $password, $method_family = null) {
+		$this->__username = $username;
+		$this->__password = $password;
+		$this->__method_family = $method_family;
+		$this->credentials = sprintf('%s:%s', $username, $password);
+	}
 
-  function request($api_url) {
-    $curl_handle = curl_init();
-    curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-    curl_setopt($curl_handle, CURLOPT_USERPWD, $this->credentials);
-    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($curl_handle);
-    $this->http_status = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
-    curl_close($curl_handle);
-    $this->last_api_call = $api_url;
-    return $response;
-  }
+	function __get($var) {
+		$class_name = get_class($this);
+		$ref = new $class_name($this->__username, $this->__password, $var);
+		return $ref;
+	}
 
+	function __call($name, $args) {
+		$fields = http_build_query((count($args) > 0) ? $args[0] : array());
+		$method = (isset($this->__method_family)) ? sprintf('%s/%s', $this->__method_family, $name) : $name;
+		$api_call = sprintf('http://api.getglue.com/v1/%s?%s', $method, $fields);
+		return $this->request($api_call);
+	}
+	
+}
+
+class Glue extends Gluestick {
+
+	protected function request($api_url) {
+		$curl_handle = curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL, $api_url);
+		curl_setopt($curl_handle, CURLOPT_USERPWD, $this->credentials);
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($curl_handle);
+		$this->http_status = curl_getinfo($curl_handle, CURLINFO_HTTP_CODE);
+		curl_close($curl_handle);
+		$this->last_api_call = $api_url;
+		return $response;
+	}
+
+}
+
+class GlueAsync extends Gluestick {
+
+	private $handles = array();
+	
+	protected function request($api_url) {
+		$curl_handle = curl_init();
+		curl_setopt($curl_handle, CURLOPT_URL, $api_url);
+		curl_setopt($curl_handle, CURLOPT_USERPWD, $this->credentials);
+		curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
+		$this->handles[] = curl_copy_handle($curl_handle);
+		return $curl_handle;
+	}
+	
+	public function exec($handles = array()) {
+		$handles = count($handles) ? $handles : $this->handles;
+		$multi_handle = curl_multi_init();
+		foreach ($handles as $handle) {
+			curl_multi_add_handle($multi_handle, $handle);
+		}
+		do {
+    		$mrc = curl_multi_exec($multi_handle, $active);
+		} while ($mrc == CURLM_CALL_MULTI_PERFORM);
+		while ($active && $mrc == CURLM_OK) {
+			if (curl_multi_select($multi_handle) != -1) {
+				do {
+					$mrc = curl_multi_exec($multi_handle, $active);
+				} while ($mrc == CURLM_CALL_MULTI_PERFORM);
+			}
+		}
+		$results = array();
+		foreach ($handles as $handle) {
+			$results[] = array(
+				'code' => curl_getinfo($handle, CURLINFO_HTTP_CODE),
+				'response' => curl_multi_getcontent($handle)
+			);
+		}
+		return $results;
+	}
+	
 }
 ?>
